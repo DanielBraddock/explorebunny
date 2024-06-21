4ab033a8897904ecfba14098752a9c3b98b3b3d4
Daniel Braddock <daniel.braddock@avantiagroup.co.uk>
2024-06-21 14:50:40 +0100

    Pass check()
    Imports
    Data masking

--- R/explore_na.R
+++ R/explore_na.R
@@ -1,4 +1,26 @@
 
+#' @title  Explore missing values in a dataset
+#'
+#' @description Primarily a visual tool, but also can tabulate the same, which allows exploration of the amount of missingness in a dataset. The visual shows the amount of missing data in each column.
+#'
+#' @param df dataset, of the class data.frame
+#' @param return Either "plot" or "table", depending what you want the function to return
+#' @param probabilities Do you want the amount missing reported in absolute (nrow) or relative (percentage)terms
+#' @param keep_all Whether to include the columns which are not missing anything ie n_missing = 0
+#' @param colour_thresholds The visuals give a RAG rating for the missingness depending on these thresholds
+#' @param zoom_in Should the amount missing axis max out at 100 percent or the greatest non-zero amount missing?
+#' @param reference_lines Should reference lines be drawn on the visual to show the thresholds used for the colours
+#'
+#' @returns A ggplot object
+#' @export
+#'
+#' @examples
+#' iris2 <- iris
+#' iris2[iris2$Species == "setosa", "Species"] <- NA_character_
+#' iris2[iris2$Sepal.Length < 5.1, "Sepal.Length"] <- NA_real_
+#' iris2[iris2$Sepal.Width > 2.8, "Sepal.Width"] <- NA_real_
+#' iris2 |> plot_na()
+#'
 explore_na <- function(df
                        , return = "plot"
                        , probabilities = TRUE
@@ -19,30 +41,30 @@ explore_na <- function(df
     tibble(
       column = colnames(df)
       , n_missing = df |> sapply(function(x) sum(is.na(x)))
-      , p_missing = n_missing / n
+      , p_missing = .data$n_missing / n
     ) |>
     mutate(
-      column = column |> factor() |> fct_reorder(n_missing)
+      column = .data$column |> factor() |> fct_reorder(n_missing)
       , rating = case_when(
         p_missing < colour_thresholds[1] ~ "green"
         , p_missing < colour_thresholds[2] ~ "orange"
         , TRUE ~ "red"
       )
     ) |>
-    arrange(desc(n_missing))
+    arrange(desc("n_missing"))
 
   # optional filter
   if (!keep_all) {
-    result <- result |> filter(n_missing > 0)
+    result <- result |> filter(.data$n_missing > 0)
   }
 
   # optionally convert to a plot (default)
   if (return == "plot") {
 
     if (probabilities) {
-      result <- result |> ggplot(aes(column, p_missing, fill = rating))
+      result <- result |> ggplot(aes(.data$column, .data$p_missing, fill = .data$rating))
     } else {
-      result <- result |> ggplot(aes(column, n_missing, fill = rating))
+      result <- result |> ggplot(aes(.data$column, .data$n_missing, fill = .data$rating))
     }
 
     result <-
